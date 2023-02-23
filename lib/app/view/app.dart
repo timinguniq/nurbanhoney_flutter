e21d1361af7f402df1a7d77f05bd977d1ee3dec0
timinguniq <timing.uniq@gmail.com>
2023-02-23 20:00:19 +0900

    feat: init

---
+++ lib/app/view/app.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const AppView();
+  }
+}
+
+class AppView extends StatefulWidget {
+  const AppView({Key? key}) : super(key: key);
+
+  @override
+  State<AppView> createState() => _AppViewState();
+}
+
+class _AppViewState extends State<AppView> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(
+        child: Text('test'),
+      ),
+    );
+  }
+}
+
