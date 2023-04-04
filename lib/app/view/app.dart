66e3f71e841b5cadc9feb14a7dc7700c2df066d2
timinguniq <timing.uniq@gmail.com>
2023-04-04 18:32:12 +0900

    feat: auth watch example

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,3 +1,7 @@
+import 'dart:async';
+
+import 'package:authentication_domain/authentication_domain.dart';
+import 'package:authentication_service/authentication_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nurbanhoney_ui_service/nurbanhoney_ui_service.dart';
@@ -24,12 +28,27 @@ class _AppViewState extends State<AppView> {
     return Consumer(
       builder: (_, WidgetRef ref, __){
         final standard = ref.watch(standardTheme);
+        final authenticationProvider = ref.watch(authenticationServiceProvider);
+        // TODO : 여기에 딜레이 걸어서 화면 변화 테스트해보기.
+        Timer(const Duration(seconds: 5), (){
+          /// 사실상 필요 없으나 예시코드.
+          ref.read(authenticationServiceProvider.notifier).state = AuthenticationStatus.authenticated;
+        });
+
         return MaterialApp(
           debugShowCheckedModeBanner: false,
           theme: standard,
-          home: Scaffold(
-            body: Text('test'),
-          ),
+          home: authenticationProvider == AuthenticationStatus.authenticated
+              ? Scaffold(
+                  body: Center(
+                    child: Text('auth'),
+                  ),
+                )
+              :Scaffold(
+                  body: Center(
+                    child: Text('unauth'),
+                  ),
+                )
         );
       },
     );
