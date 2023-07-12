b99b3a25e36fa01622e9e2ab09d80a40d5df603e
timinguniq <timing.uniq@gmail.com>
2023-07-12 12:35:23 +0900

    add: kakao login init(2)

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 
 import 'app/app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+
+  KakaoSdk.init(
+    nativeAppKey: ''
+  );
+
   runApp(const ProviderScope(
     child: App(),
   ));
