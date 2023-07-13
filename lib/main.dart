c3f39aafa479c36504e13cee5f239fe516b5e8ab
timinguniq <timing.uniq@gmail.com>
2023-07-13 17:11:22 +0900

    add: kakaoNativeAppKey

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
+import 'package:nurbanhoney/config/config.dart';
 
 import 'app/app.dart';
 
@@ -8,7 +9,7 @@ void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
   KakaoSdk.init(
-    nativeAppKey: ''
+    nativeAppKey: kakaoNativeAppKey,
   );
 
   runApp(const ProviderScope(
