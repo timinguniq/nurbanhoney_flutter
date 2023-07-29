de6c97034cd2189d15dee1a5670076a57848a73b
timinguniq <timing.uniq@gmail.com>
2023-07-29 17:35:10 +0900

    add: profile_repository init

--- data/dio_repository/lib/src/profile_repository.dart
+++ data/dio_repository/lib/src/profile_repository.dart
@@ -1,88 +1,69 @@
+import 'dart:developer';
 
 import 'package:dio/dio.dart';
+import 'package:dio_domain/dio_domain.dart';
 
 /// TODO 여기서 부터고 insigniaShow, insighinaOwn 아마 List로 안 받아질 듯.
-typedef ProfileType = ({int id, String loginType,
-  String badge, String nickname, String description,
-  int point, String insigniaShow, String insigniaOwn,
-  int myArticleCount, int myCommentCount});
-
+typedef ProfileType = ({
+  int id,
+  String loginType,
+  String badge,
+  String nickname,
+  String description,
+  int point,
+  String insigniaShow,
+  String insigniaOwn,
+  int myArticleCount,
+  int myCommentCount
+});
 
 class ProfileRepository {
   /// constructor
   ProfileRepository._privateConstructor();
-  static final ProfileRepository _instance = ProfileRepository._privateConstructor();
 
-  factory ProfileRepository(){
+  static final ProfileRepository _instance =
+      ProfileRepository._privateConstructor();
+
+  factory ProfileRepository() {
     return _instance;
   }
 
   final dio = Dio();
 
   // 참고해서 getProfile 만들기
-  Future<List<({int id, int type, String name, String address})>> getBoard() async {
-    try {
-      final response = await dio.get('${DioApi.mainApi}/board');
-      final result = <({int id, int type, String name, String address})>[];
-      for(int i = 0; i < response.data.length ; i++) {
-        log('getBoard response: ${response.data[i]}');
-        final records =
-        (id: int.parse(response.data[i]['id'].toString()),
-        type: int.parse(response.data[i]['type'].toString()),
-        name: response.data[i]['name'].toString(),
-        address: response.data[i]['address'].toString());
-        // result.add(BoardModel.fromJson(response.data[i]));
-        result.add(records);
-      }
-      log('getBoard response: ${response.data.toString()}');
-
-      final futureValue = Future.value(result);
-      return futureValue;
-    } catch (e) {
-      log('getBoard error : $e');
-      throw Exception(e);
-    }
-  }
-
-  Future<List<BoardAllType>> getBoardAll({
-    required int flag,
-    required int articleId,
-    required int limit,
+  Future<ProfileType> getProfile({
+    required String token,
   }) async {
     try {
-      final response = await dio.get(
-        '${DioApi.mainApi}/board/all',
-        queryParameters: {
-          'flag': flag,
-          'articleId': articleId,
-          'limit': limit,
-        },
+      final baseOptions = BaseOptions(
+        baseUrl: '${DioApi.mainApi}/profile',
+        headers: {'Authorization': 'Bearer $token'},
+        connectTimeout: const Duration(seconds: 5),
+        receiveTimeout: const Duration(seconds: 3),
       );
-      final result = <BoardAllType>[];
-      for(int i = 0; i < response.data.length ; i++) {
-        log('getBoardAll response: ${response.data[i]}');
-        final records =
-        (id: int.parse(response.data[i]['id'].toString()),
-        board: int.parse(response.data[i]['board'].toString()),
-        thumbnail: response.data[i]['thumbnail'].toString(),
-        title: response.data[i]['title'].toString(),
-        lossCut: response.data[i]['lossCut'].toString(),
-        content: response.data[i]['content'].toString(),
-        commentCount: response.data[i]['commentCount'].toString(),
-        likeCount: response.data[i]['likeCount'].toString(),
-        createdAt: response.data[i]['createdAt'].toString(),
-        nickname: response.data[i]['user']['nickname'].toString());
-        // result.add(BoardModel.fromJson(response.data[i]));
-        result.add(records);
-      }
-      log('getBoardAll response: ${response.data.toString()}');
+
+      final authDio = Dio(baseOptions);
+      final response = await authDio.get('/');
+
+      final result = (
+        id: int.parse(response.data['id'].toString()),
+        loginType: response.data['loginType'].toString(),
+        badge: response.data['badge'].toString(),
+        nickname: response.data['nickname'].toString(),
+        description: response.data['description'].toString(),
+        point: int.parse(response.data['point'].toString()),
+        insigniaShow: response.data['insigniaShow'].toString(),
+        insigniaOwn: response.data['insigniaOwn'].toString(),
+        myArticleCount: int.parse(response.data['myArticleCount'].toString()),
+        myCommentCount: int.parse(response.data['myCommentCount'].toString())
+      );
+      log('getProfile result: ${result.toString()}');
 
       final futureValue = Future.value(result);
       return futureValue;
     } catch (e) {
-      log('getBoardAll error : $e');
+      log('getProfile error : $e');
       throw Exception(e);
     }
   }
-
 }
