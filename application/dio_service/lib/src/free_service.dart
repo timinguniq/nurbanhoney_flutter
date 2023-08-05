039d42fa8cc884d0ed02775ce0f571b3b1f01de4
timinguniq <timing.uniq@gmail.com>
2023-08-05 09:40:50 +0900

    add: nurban, free service

--- application/dio_service/lib/src/free_service.dart
+++ application/dio_service/lib/src/free_service.dart
@@ -1,6 +1,12 @@
 import 'package:dio_repository/dio_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+final getFreeArticleProvider =
+    FutureProvider.autoDispose.family<FreeArticle, int>((ref, articleId) async {
+  final freeRepository = ref.watch(freeRepositoryProvider);
+  return await freeRepository.getFreeArticle(articleId: articleId);
+});
+
 final freeRepositoryProvider = Provider<FreeRepository>((ref) {
   return FreeRepository();
 });
--- application/dio_service/lib/src/nurban_service.dart
+++ application/dio_service/lib/src/nurban_service.dart
@@ -1,6 +1,12 @@
 import 'package:dio_repository/dio_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+final getNurbanArticleProvider =
+    FutureProvider.autoDispose.family<NurbanArticle, int>((ref, articleId) async {
+  final nurbanRepository = ref.watch(nurbanRepositoryProvider);
+  return await nurbanRepository.getNurbanArticle(articleId: articleId);
+});
+
 final nurbanRepositoryProvider = Provider<NurbanRepository>((ref) {
   return NurbanRepository();
 });
