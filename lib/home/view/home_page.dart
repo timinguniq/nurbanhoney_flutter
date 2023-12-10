7269de22787b784c8b5f2f2de5c36b633cc1c56d
timinguniq <timing.uniq@gmail.com>
2023-12-10 09:16:35 +0900

    add: homepage route

--- lib/home/view/home_page.dart
+++ lib/home/view/home_page.dart
@@ -13,28 +13,55 @@ import 'package:nurbanhoney/login/login.dart';
 import 'package:nurbanhoney_ui_service/nurbanhoney_ui_service.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({
+    required HomeBottomStatus homeBottomStatus,
+    required HomeAppbarStatus homeAppbarStatus,
+    Key? key,
+  })  : _homeBottomStatus = homeBottomStatus,
+        _homeAppbarStatus = homeAppbarStatus,
+        super(key: key);
 
-  static Route route() {
+  final HomeBottomStatus _homeBottomStatus;
+  final HomeAppbarStatus _homeAppbarStatus;
+
+  static Route route({
+    HomeBottomStatus homeBottomStatus = HomeBottomStatus.home,
+    HomeAppbarStatus homeAppbarStatus = HomeAppbarStatus.whole,
+  }) {
     return MaterialPageRoute<void>(
-      builder: (_) => const HomePage(),
+      builder: (_) => HomePage(
+        homeBottomStatus: homeBottomStatus,
+        homeAppbarStatus: homeAppbarStatus,
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return const HomeView();
+    return HomeView(
+      homeBottomStatus: _homeBottomStatus,
+      homeAppbarStatus: _homeAppbarStatus,
+    );
   }
 }
 
-class HomeView extends StatefulWidget {
-  const HomeView({Key? key}) : super(key: key);
+class HomeView extends ConsumerStatefulWidget {
+  const HomeView({
+    required HomeBottomStatus homeBottomStatus,
+    required HomeAppbarStatus homeAppbarStatus,
+    Key? key,
+  })  : _homeBottomStatus = homeBottomStatus,
+        _homeAppbarStatus = homeAppbarStatus,
+        super(key: key);
+
+  final HomeBottomStatus _homeBottomStatus;
+  final HomeAppbarStatus _homeAppbarStatus;
 
   @override
-  State<HomeView> createState() => _HomeViewState();
+  ConsumerState<HomeView> createState() => _HomeViewState();
 }
 
-class _HomeViewState extends State<HomeView> {
+class _HomeViewState extends ConsumerState<HomeView> {
   int _selectedIndex = 0;
 
   final List<Widget> _widgetOptions = <Widget>[
@@ -42,6 +69,47 @@ class _HomeViewState extends State<HomeView> {
     HoneyRankBodyView(),
     MyaccountBodyView(),
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    switch (widget._homeBottomStatus) {
+      case HomeBottomStatus.home:
+        _selectedIndex = 0;
+        break;
+      case HomeBottomStatus.rank:
+        _selectedIndex = 1;
+        break;
+      case HomeBottomStatus.myaccount:
+        _selectedIndex = 2;
+        break;
+      case HomeBottomStatus.unknown:
+        _selectedIndex = 0;
+        break;
+    }
+    switch (widget._homeAppbarStatus) {
+      case HomeAppbarStatus.whole:
+        ref.read(homeAppbarNavigationProvider.notifier)
+           .select(HomeAppbarStatus.whole);
+        break;
+      case HomeAppbarStatus.popular:
+        ref.read(homeAppbarNavigationProvider.notifier)
+            .select(HomeAppbarStatus.popular);
+        break;
+      case HomeAppbarStatus.nurban:
+        ref.read(homeAppbarNavigationProvider.notifier)
+            .select(HomeAppbarStatus.nurban);
+        break;
+      case HomeAppbarStatus.free:
+        ref.read(homeAppbarNavigationProvider.notifier)
+            .select(HomeAppbarStatus.free);
+        break;
+      case HomeAppbarStatus.unknown:
+        ref.read(homeAppbarNavigationProvider.notifier)
+            .select(HomeAppbarStatus.whole);
+        break;
+    }
+  }
 
   void _onItemTapped(int index) {
     setState(() {
@@ -51,37 +119,35 @@ class _HomeViewState extends State<HomeView> {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer(builder: (_, WidgetRef ref, __) {
-      final floatButtonColor = ref.read(colorF6B748);
-      final authenticationProvider = ref.watch(authenticationServiceProvider);
+    final floatButtonColor = ref.read(colorF6B748);
+    final authenticationProvider = ref.watch(authenticationServiceProvider);
 
-      final homeBottomNavigation = ref.watch(homeBottomNavigationProvider);
-
-      return Scaffold(
-        extendBody: true,
-        appBar: _selectedIndex != 2
-            ? PreferredSize(
-                preferredSize: const Size.fromHeight(52),
-                child: AppBar(
-                  iconTheme: const IconThemeData(color: Colors.black),
-                  backgroundColor: Colors.white,
-                  elevation: 0,
-                  title: Row(
-                    children: [
-                      SizedBox(
-                        width: 30,
-                        height: 25,
-                        child: Assets.images.home.nurbanSymbol.image(),
-                      ),
-                      const SizedBox(width: 12),
-                      SizedBox(
-                        width: 55,
-                        height: 20,
-                        child: Assets.images.home.nurbanChar.image(),
-                      ),
-                    ],
-                  ),
-                  /*
+    final homeBottomNavigation = ref.watch(homeBottomNavigationProvider);
+    return Scaffold(
+      extendBody: true,
+      appBar: _selectedIndex != 2
+          ? PreferredSize(
+              preferredSize: const Size.fromHeight(52),
+              child: AppBar(
+                iconTheme: const IconThemeData(color: Colors.black),
+                backgroundColor: Colors.white,
+                elevation: 0,
+                title: Row(
+                  children: [
+                    SizedBox(
+                      width: 30,
+                      height: 25,
+                      child: Assets.images.home.nurbanSymbol.image(),
+                    ),
+                    const SizedBox(width: 12),
+                    SizedBox(
+                      width: 55,
+                      height: 20,
+                      child: Assets.images.home.nurbanChar.image(),
+                    ),
+                  ],
+                ),
+                /*
                   actions: [
                     IconButton(
                       onPressed: () {
@@ -96,85 +162,84 @@ class _HomeViewState extends State<HomeView> {
                   ],
 
                    */
-                ),
-              )
-            : PreferredSize(
-                preferredSize: const Size.fromHeight(52),
-                child: Padding(
-                  padding:
-                      EdgeInsets.only(top: MediaQuery.of(context).padding.top),
-                  child: const Center(child: Text('마이페이지')),
-                ),
               ),
-        endDrawer: const DrawerProfile(),
-        body: _widgetOptions.elementAt(_selectedIndex),
-        floatingActionButton: _selectedIndex != 2
-            ? FloatingActionButton(
-                onPressed: () {
-                  if (authenticationProvider ==
-                      AuthenticationStatus.authenticated) {
-                    //Navigator.of(context).push(BoardPage.routeName);
-                    Navigator.of(context).push(ArticleCreatePage.route());
-                  } else {
-                    Navigator.of(context).push(LoginPage.route());
-                  }
-                },
-                backgroundColor: floatButtonColor,
-                child: SizedBox(
-                  width: 32,
-                  height: 32,
-                  child: Assets.images.home.floatingButton.image(),
-                ),
-              )
-            : null,
-        bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: Colors.white,
-          items: [
-            BottomNavigationBarItem(
-              icon: SizedBox(
-                width: 32,
-                height: 32,
-                child: Assets.images.home.naviHomeInactive.image(),
+            )
+          : PreferredSize(
+              preferredSize: const Size.fromHeight(52),
+              child: Padding(
+                padding:
+                    EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+                child: const Center(child: Text('마이페이지')),
               ),
-              activeIcon: SizedBox(
-                width: 32,
-                height: 32,
-                child: Assets.images.home.naviHomeActive.image(),
-              ),
-              label: '홈',
             ),
-            BottomNavigationBarItem(
-              icon: SizedBox(
+      endDrawer: const DrawerProfile(),
+      body: _widgetOptions.elementAt(_selectedIndex),
+      floatingActionButton: _selectedIndex != 2
+          ? FloatingActionButton(
+              onPressed: () {
+                if (authenticationProvider ==
+                    AuthenticationStatus.authenticated) {
+                  //Navigator.of(context).push(BoardPage.routeName);
+                  Navigator.of(context).push(ArticleCreatePage.route());
+                } else {
+                  Navigator.of(context).push(LoginPage.route());
+                }
+              },
+              backgroundColor: floatButtonColor,
+              child: SizedBox(
                 width: 32,
                 height: 32,
-                child: Assets.images.home.naviRankInactive.image(),
+                child: Assets.images.home.floatingButton.image(),
               ),
-              activeIcon: SizedBox(
-                width: 32,
-                height: 32,
-                child: Assets.images.home.naviRankActive.image(),
-              ),
-              label: '꿀랭킹',
+            )
+          : null,
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        items: [
+          BottomNavigationBarItem(
+            icon: SizedBox(
+              width: 32,
+              height: 32,
+              child: Assets.images.home.naviHomeInactive.image(),
             ),
-            BottomNavigationBarItem(
-              icon: SizedBox(
-                width: 32,
-                height: 32,
-                child: Assets.images.home.naviMyaccountInactive.image(),
-              ),
-              activeIcon: SizedBox(
-                width: 32,
-                height: 32,
-                child: Assets.images.home.naviMyaccountActive.image(),
-              ),
-              label: '내 정보',
+            activeIcon: SizedBox(
+              width: 32,
+              height: 32,
+              child: Assets.images.home.naviHomeActive.image(),
             ),
-          ],
-          currentIndex: _selectedIndex,
-          selectedItemColor: floatButtonColor,
-          onTap: _onItemTapped,
-        ),
-      );
-    });
+            label: '홈',
+          ),
+          BottomNavigationBarItem(
+            icon: SizedBox(
+              width: 32,
+              height: 32,
+              child: Assets.images.home.naviRankInactive.image(),
+            ),
+            activeIcon: SizedBox(
+              width: 32,
+              height: 32,
+              child: Assets.images.home.naviRankActive.image(),
+            ),
+            label: '꿀랭킹',
+          ),
+          BottomNavigationBarItem(
+            icon: SizedBox(
+              width: 32,
+              height: 32,
+              child: Assets.images.home.naviMyaccountInactive.image(),
+            ),
+            activeIcon: SizedBox(
+              width: 32,
+              height: 32,
+              child: Assets.images.home.naviMyaccountActive.image(),
+            ),
+            label: '내 정보',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: floatButtonColor,
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
