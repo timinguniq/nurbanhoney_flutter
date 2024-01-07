35c2bb1068b94ef01970cc237c0d8b554878165f
timinguniq <timing.uniq@gmail.com>
2024-01-07 11:20:30 +0900

    add: myaccount tab status

--- domain/navigation_domain/lib/src/models/models.dart
+++ domain/navigation_domain/lib/src/models/models.dart
@@ -1,3 +1,3 @@
 export 'home_appbar_status.dart';
 export 'home_bottom_status.dart';
-
+export 'myaccount_tab_status.dart';
---
+++ domain/navigation_domain/lib/src/models/myaccount_tab_status.dart
@@ -0,0 +1,11 @@
+/// Status to manage myaccount tab navigation
+enum MyaccountTabStatus{
+  /// initial status
+  unknown,
+
+  /// article
+  article,
+
+  /// comment
+  comment,
+}
