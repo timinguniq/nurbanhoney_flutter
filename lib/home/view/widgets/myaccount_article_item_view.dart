b3af8e10930aec2fcd076f4c445d0b318b8bdc44
timinguniq <timing.uniq@gmail.com>
2024-01-13 16:19:10 +0900

    add: myaccount article item viewe

--- lib/home/view/widgets/myaccount_article_item_view.dart
+++ lib/home/view/widgets/myaccount_article_item_view.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nurbanhoney/article_create/article_create.dart';
+import 'package:nurbanhoney_ui_service/nurbanhoney_ui_service.dart';
 
 // Consumer widget format
 // TODO: myaccount article item view를 구현해야됨.
 class MyaccountArticleItemView extends StatelessWidget {
-  const MyaccountArticleItemView({
-    required String title,
-    required String data,
-    required String count,
-    required String commentCount,
-    super.key
-  }): _title = title,
-      _data = data,
-      _count = count,
-      _commentCount = commentCount;
+  const MyaccountArticleItemView(
+      {required String title,
+      required String data,
+      required String count,
+      required String commentCount,
+      super.key})
+      : _title = title,
+        _data = data,
+        _count = count,
+        _commentCount = commentCount;
 
   final String _title;
   final String _data;
@@ -27,40 +28,64 @@ class MyaccountArticleItemView extends StatelessWidget {
       //final floatButtonColor = ref.read(colorF6B748);
       //final authenticationProvider = ref.watch(authenticationServiceProvider);
 
-      return SizedBox(
-        width: double.infinity,
-        height: 75,
-        child: Column(
-          children: [
-            Row(
+      final dividerColor = ref.read(colorBBBBBB);
+      final commentCountBorderColor = ref.read(color808080);
+      final titleTextStyle = ref.read(myaccountArticleItemTitleStyle);
+      final dataTextStyle = ref.read(myaccountArticleItemDateStyle);
+      final commentCountTextStyle =
+          ref.read(myaccountArticleItemCommentCountStyle);
+
+      return Column(
+        children: [
+          SizedBox(
+            width: double.infinity,
+            height: 75,
+            child: Row(
               children: [
                 Column(
                   children: [
-
+                    Text(
+                      _title,
+                      style: titleTextStyle,
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          _data,
+                          style: dataTextStyle,
+                        ),
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        Text(
+                          '∙ 조회수 $_count',
+                          style: dataTextStyle,
+                        ),
+                      ],
+                    ),
                   ],
                 ),
-                // TODO : commnetCount
                 Container(
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(5),
-                    color: Colors.grey,
+                    color: commentCountBorderColor,
                   ),
                   child: Text(
                     _commentCount,
-                    style: TextStyle(
-                      fontSize: 12,
-                      color: Colors.white,
-                    ),
+                    style: commentCountTextStyle,
                   ),
                 )
               ],
             ),
-            ArticleCreateDivider(
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: ArticleCreateDivider(
               thickness: 1,
-              color: Colors.grey,
+              color: dividerColor,
             ),
-          ],
-        ),
+          ),
+        ],
       );
     });
   }
