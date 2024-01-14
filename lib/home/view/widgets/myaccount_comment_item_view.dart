36a1fe188237cbd38a6676d645e88332a6308c60
timinguniq <timing.uniq@gmail.com>
2024-01-14 12:41:19 +0900

    add: myaccount comment item view(1)

--- lib/home/view/widgets/myaccount_comment_item_view.dart
+++ lib/home/view/widgets/myaccount_comment_item_view.dart
@@ -1,18 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:nurbanhoney/article_create/article_create.dart';
+import 'package:nurbanhoney/gen/assets.gen.dart';
+import 'package:nurbanhoney_ui_service/nurbanhoney_ui_service.dart';
 
-// Consumer widget format
-// TODO: myaccount comment item view를 구현해야됨.
 class MyaccountCommentItemView extends StatelessWidget {
-  const MyaccountCommentItemView({super.key});
+  const MyaccountCommentItemView(
+      {required String title, required String data, super.key})
+      : _title = title,
+        _data = data;
+
+  final String _title;
+  final String _data;
 
   @override
   Widget build(BuildContext context) {
     return Consumer(builder: (_, WidgetRef ref, __) {
-      //final floatButtonColor = ref.read(colorF6B748);
-      //final authenticationProvider = ref.watch(authenticationServiceProvider);
+      final dividerColor = ref.read(colorBBBBBB);
 
-      return Container();
+      final titleTextStyle = ref.read(myaccountArticleItemTitleStyle);
+      final dataTextStyle = ref.read(myaccountArticleItemDateStyle);
+
+      return Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: SizedBox(
+              width: double.infinity,
+              height: 75,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        _title,
+                        style: titleTextStyle,
+                      ),
+                      const SizedBox(
+                        height: 4,
+                      ),
+                      Text(
+                        _data,
+                        style: dataTextStyle,
+                      ),
+                    ],
+                  ),
+                  InkWell(
+                    onTap: (){
+
+                    },
+                    child: Assets.images.articleDetail.deleteIcon.image(),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: ArticleCreateDivider(
+              thickness: 1,
+              color: dividerColor,
+            ),
+          ),
+        ],
+      );
     });
   }
 }
