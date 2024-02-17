fd26778ab5e92225c27ec0bf95162d304fa268dd
timinguniq <timing.uniq@gmail.com>
2024-02-17 09:19:35 +0900

    feat: myaccount_edit_insigniaown_widget init(2)

---
+++ lib/myaccount_edit/view/widgets/myaccount_edit_insignia_own_widget.dart
@@ -0,0 +1,90 @@
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:nurbanhoney_ui_service/nurbanhoney_ui_service.dart';
+
+/// 내 정보 수정화면 소유한 휘장
+class MyaccountEditInsigniaOwnWidget extends StatelessWidget {
+  const MyaccountEditInsigniaOwnWidget({
+    required String insigniaShow,
+    super.key,
+  }) : _insigniaShow = insigniaShow;
+
+  final String _insigniaShow;
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer(builder: (_, WidgetRef ref, __) {
+      final titleStyle = ref.read(myaccountEditTitleStyle);
+      final confirmStyle = ref.read(myaccountEditConfirmStyle);
+      final subTitleStyle = ref.read(myaccountEditSubTitleStyle);
+      final subValueStyle = ref.read(myaccountEditSubValueStyle);
+      final warnStyle = ref.read(myaccountEditWarnStyle);
+
+      final insigniaShowList = _insigniaShow.split(',');
+
+      return SizedBox(
+          width: double.infinity,
+          height: 110,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 19, left: 16),
+                child: Text(
+                  '보이는 휘장',
+                  style: subTitleStyle,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 16, top: 9),
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      for (var ele in insigniaShowList)
+                        Padding(
+                          padding: const EdgeInsets.only(right: 3),
+                          child: InkWell(
+                            onTap: () {
+                              // TODO: 클릭시 삭제
+                            },
+                            child: CachedNetworkImage(
+                              imageUrl: ele,
+                              imageBuilder: (context, imageProvider) =>
+                                  Container(
+                                    width: 30,
+                                    height: 30,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(5),
+                                      image: DecorationImage(
+                                        image: imageProvider,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                              placeholder: (context, url) =>
+                              const CircularProgressIndicator(),
+                              errorWidget: (context, url, error) =>
+                              const Icon(Icons.error),
+                            ),
+                          ),
+                        ),
+                    ],
+                  ),
+                ),
+              ),
+              if(insigniaShowList.isNotEmpty)
+                Padding(
+                  padding: const EdgeInsets.only(left: 16, top: 8),
+                  child: Text(
+                    '2개 이상은 선택할 수 없습니다.',
+                    style: warnStyle,
+                  ),
+                ),
+            ],
+          ));
+    });
+  }
+}
