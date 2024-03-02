4e0e1f0dce982391b44dc7604431ff167da68924
timinguniq <timing.uniq@gmail.com>
2024-03-02 12:41:31 +0900

    feat: myaccountEditConfirm1

--- lib/myaccount_edit/view/widgets/myaccount_edit_title_widget.dart
+++ lib/myaccount_edit/view/widgets/myaccount_edit_title_widget.dart
@@ -2,9 +2,12 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:navigation_service/navigation_service.dart';
 import 'package:nurbanhoney/gen/assets.gen.dart';
 import 'package:nurbanhoney_ui_service/nurbanhoney_ui_service.dart';
+
+import '../myaccount_edit_page.dart';
 
 /// 내 정보 수정화면 타이틀
 class MyaccountEditTitleWidget extends StatelessWidget {
@@ -61,9 +64,20 @@ class MyaccountEditTitleWidget extends StatelessWidget {
                 onTap: () {
                   log('저장 버튼 클릭');
                   log('myaccountEditConfirm : $myaccountEditConfirm');
+                  log('insigniaShowList : $insigniaShowList');
                   // 만일 nickname과 description가 빈 스트링이 아니라면 저장
                   if(myaccountEditConfirm){
 
+                  }else{
+                    Fluttertoast.showToast(
+                      msg: '닉네임과 자기소개를 입력해주세요.',
+                      toastLength: Toast.LENGTH_SHORT,
+                      gravity: ToastGravity.BOTTOM,
+                      timeInSecForIosWeb: 1,
+                      backgroundColor: Colors.black,
+                      textColor: Colors.white,
+                      fontSize: 16.0,
+                    );
                   }
                 },
                 child: Padding(
