0ec6dce1b4163daeb9d767944f1fd8cc16720e78
timinguniq <timing.uniq@gmail.com>
2024-03-02 12:51:53 +0900

    feat: myaccountEdit init

--- lib/myaccount_edit/view/widgets/myaccount_edit_title_widget.dart
+++ lib/myaccount_edit/view/widgets/myaccount_edit_title_widget.dart
@@ -1,11 +1,13 @@
 import 'dart:developer';
 
+import 'package:dio_service/dio_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:navigation_service/navigation_service.dart';
 import 'package:nurbanhoney/gen/assets.gen.dart';
 import 'package:nurbanhoney_ui_service/nurbanhoney_ui_service.dart';
+import 'package:preference_storage_service/preference_storage_service.dart';
 
 import '../myaccount_edit_page.dart';
 
@@ -23,6 +25,14 @@ class MyaccountEditTitleWidget extends StatelessWidget {
       final warnStyle = ref.read(myaccountEditWarnStyle);
 
       final myaccountEditConfirm = ref.watch(myaccountEditProvider);
+      final profileRepository = ref.read(profileRepositoryProvider);
+
+      final preferenceStorage = ref.watch(preferenceStorageProvider);
+      final storage = preferenceStorage.asData?.value;
+      final token = storage?.getToken() ?? '__empty__';
+
+      final nickname = ref.watch(myaccountEditNicknameNavigationProvider);
+      final description = ref.watch(myaccountEditDescriptionNavigationProvider);
 
 
       return SizedBox(
@@ -61,13 +71,18 @@ class MyaccountEditTitleWidget extends StatelessWidget {
               top: 0,
               bottom: 0,
               child: InkWell(
-                onTap: () {
+                onTap: () async {
                   log('저장 버튼 클릭');
                   log('myaccountEditConfirm : $myaccountEditConfirm');
                   log('insigniaShowList : $insigniaShowList');
                   // 만일 nickname과 description가 빈 스트링이 아니라면 저장
                   if(myaccountEditConfirm){
-
+                    await profileRepository.profileEdit(
+                        token: token,
+                        nickname: nickname,
+                        description: description,
+                        insignia: insigniaShowList.toString(),
+                    );
                   }else{
                     Fluttertoast.showToast(
                       msg: '닉네임과 자기소개를 입력해주세요.',
