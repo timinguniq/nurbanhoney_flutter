c4f6eee7446c3da68b7b86bb7ba5ffc408ad75b4
timinguniq <timing.uniq@gmail.com>
2024-05-05 18:42:54 +0900

    feat: backup

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -2,6 +2,8 @@ import 'package:authentication_domain/authentication_domain.dart';
 import 'package:authentication_service/authentication_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:navigation_domain/navigation_domain.dart';
+import 'package:nurbanhoney/home/home.dart';
 import 'package:nurbanhoney/splash/splash.dart';
 import 'package:nurbanhoney_ui_service/nurbanhoney_ui_service.dart';
 
@@ -40,8 +42,14 @@ class _AppViewState extends State<AppView> {
           theme: standard,
           onGenerateRoute: (_) => SplashPage.route(),
           home: authenticationProvider == AuthenticationStatus.authenticated
-              ? const SplashPage()
-              : const SplashPage()
+              ? const HomePage(
+                  homeBottomStatus: HomeBottomStatus.home,
+                  homeAppbarStatus: HomeAppbarStatus.whole,
+                )
+              : const HomePage(
+                  homeBottomStatus: HomeBottomStatus.home,
+                  homeAppbarStatus: HomeAppbarStatus.whole,
+                )
         );
       },
     );
