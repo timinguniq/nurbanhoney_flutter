18aaa821fd0e7b49a520e609e6cb980d94dfac40
timinguniq <timing.uniq@gmail.com>
2024-06-06 18:53:41 +0900

    feat: banner ad init

--- lib/article_detail/view/article_detail_page.dart
+++ lib/article_detail/view/article_detail_page.dart
@@ -1,14 +1,14 @@
 import 'dart:developer';
+import 'dart:io';
 
-import 'package:dio_service/dio_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:nurbanhoney/article_detail/article_detail.dart';
-import 'package:nurbanhoney/gen/assets.gen.dart';
 import 'package:nurbanhoney_ui_service/nurbanhoney_ui_service.dart';
-import 'package:share_service/share_service.dart';
+import 'package:preference_storage_service/preference_storage_service.dart';
 
-class ArticleDetailPage extends StatelessWidget {
+class ArticleDetailPage extends StatefulWidget {
   const ArticleDetailPage(
       {required int board, required int articleId, super.key})
       : _board = board,
@@ -30,6 +30,20 @@ class ArticleDetailPage extends StatelessWidget {
   }
 
   @override
+  State<ArticleDetailPage> createState() => _ArticleDetailPageState();
+}
+
+class _ArticleDetailPageState extends State<ArticleDetailPage> {
+  BannerAd? _bannerAd;
+  bool _bannerAdIsLoaded = false;
+
+  AdManagerBannerAd? _adManagerBannerAd;
+  bool _adManagerBannerAdIsLoaded = false;
+
+  NativeAd? _nativeAd;
+  bool _nativeAdIsLoaded = false;
+
+  @override
   Widget build(BuildContext context) {
     return Consumer(builder: (_, WidgetRef ref, __) {
       //final floatButtonColor = ref.read(colorF6B748);
@@ -39,9 +53,16 @@ class ArticleDetailPage extends StatelessWidget {
 
       final appBarTitleStyle = ref.read(articleDetailAppbarTitleStyle);
 
+      final prefStorageProvider = ref.watch(preferenceStorageProvider);
+      final prefStorage = prefStorageProvider.asData?.value;
+
+      final androidId = prefStorage?.getDeviceId();
+
+
+
       return Scaffold(
         appBar: ArticleDetailAppBar(
-          board: _board,
+          board: widget._board,
           appBarTitleStyle: appBarTitleStyle,
         ),
         body: SingleChildScrollView(
@@ -52,51 +73,95 @@ class ArticleDetailPage extends StatelessWidget {
                 color: thinDividerColor,
               ),
               // TitleBoard
-              _board == 1
+              widget._board == 1
                   ? NurbanTitleBoard(
-                      articleId: _articleId,
+                      articleId: widget._articleId,
                     )
-                  : _board == 2
+                  : widget._board == 2
                       ? FreeTitleBoard(
-                          articleId: _articleId,
+                          articleId: widget._articleId,
                         )
                       : FreeTitleBoard(
-                          articleId: _articleId,
+                          articleId: widget._articleId,
                         ),
               ArticleDetailDivider(
                 thickness: 0.5,
                 color: thinDividerColor,
               ),
               // Content
-              _board == 1
+              widget._board == 1
                   ? NurbanContentBoard(
-                      articleId: _articleId,
+                      articleId: widget._articleId,
                     )
-                  : _board == 2
+                  : widget._board == 2
                       ? FreeContentBoard(
-                          articleId: _articleId,
+                          articleId: widget._articleId,
                         )
                       : FreeContentBoard(
-                          articleId: _articleId,
+                          articleId: widget._articleId,
                         ),
+              // 광고 banner
+              if(_bannerAdIsLoaded && _bannerAd != null)
+                Container(
+                  alignment: Alignment.center,
+                  width: _bannerAd!.size.width.toDouble(),
+                  height: _bannerAd!.size.height.toDouble(),
+                  child: AdWidget(ad: _bannerAd!),
+                ),
+              //
               ArticleDetailDivider(
                 thickness: 0.5,
                 color: thinDividerColor,
               ),
               LikeDislikeBoard(
-                articleId: _articleId,
+                articleId: widget._articleId,
               ),
               ArticleDetailDivider(
                 thickness: 8,
                 color: thickDividerColor,
               ),
               ArticleDetailCommentPage(
-                articleId: _articleId,
+                articleId: widget._articleId,
               ),
+
             ],
           ),
         ),
       );
     });
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    // Create the ad objects and load ads.
+    _bannerAd = BannerAd(
+        size: AdSize.banner,
+        adUnitId: Platform.isAndroid
+            ? 'ca-app-pub-9818502417467314/8723806743'
+            : 'ca-app-pub-3940256099942544/2934735716',
+        listener: BannerAdListener(
+          onAdLoaded: (Ad ad) {
+            log('$BannerAd loaded.');
+            setState(() {
+              _bannerAdIsLoaded = true;
+            });
+          },
+          onAdFailedToLoad: (Ad ad, LoadAdError error) {
+            log('$BannerAd failedToLoad: $error');
+            ad.dispose();
+          },
+          onAdOpened: (Ad ad) => log('$BannerAd onAdOpened.'),
+          onAdClosed: (Ad ad) => log('$BannerAd onAdClosed.'),
+        ),
+        request: const AdRequest())
+      ..load();
+
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _bannerAd?.dispose();
   }
 }
