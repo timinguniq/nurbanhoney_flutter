0de4fd8113c5e3983fa4a4a7ef62a55667b90a93
timing.uniq <timing.uniq@gmail.com>
2024-08-03 14:40:06 +0900

    add: home_page tab bar init

--- lib/home/view/home_page_refactor.dart
+++ lib/home/view/home_page_refactor.dart
@@ -64,96 +64,79 @@ class _HomeViewRefactorState extends ConsumerState<HomeViewRefactor> {
 
     final homeBottomNavigation = ref.watch(homeBottomNavigationProvider);
     return DefaultTabController(
-      length: 3,
+      length: 2,
       child: Scaffold(
-          appBar: AppBar(title: Text('Tab View With Tabbar')),
-          body: Column(
-            children: [
-              const TabBar(
-                  indicatorColor: Colors.white,
-                  labelStyle: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20),
-                  indicatorWeight: 3,
-                  tabs: [
-                    Tab(
-                      text: 'List',
-                      height: 50,
-                    ),
-                    Tab(
-                      text: 'Grid',
-                      height: 50,
-                    ),
-                    Tab(
-                      text: 'Box',
-                      height: 50,
-                    ),
-                  ]),
-              Expanded(
-                  child: TabBarView(children: [
-                    ListView.builder(
-                        key: const PageStorageKey("LIST_VIEW"),
-                        itemCount: 1000,
-                        itemBuilder: (context, index) {
-                          return Container(
-                            padding: const EdgeInsets.symmetric(vertical: 12),
-                            width: MediaQuery.of(context).size.width,
-                            child: Center(
-                              child: Text(
-                                "List View $index",
-                                style: TextStyle(
-                                    fontSize: 16,
-                                    color: Colors.accents[index % 15],
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                          );
-                        }),
-                    GridView.builder(
-                        key: const PageStorageKey("GRID_VIEW"),
-                        itemCount: 1000,
-                        gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 3,
-                          crossAxisSpacing: 12,
-                          mainAxisSpacing: 12,
-                        ),
-                        itemBuilder: ((context, index) {
-                          List<int> _number = [
-                            Random().nextInt(255),
-                            Random().nextInt(255),
-                            Random().nextInt(255)
-                          ];
-                          return Container(
-                            color: Color.fromRGBO(
-                                _number[0], _number[1], _number[2], 1),
-                            child: Center(
-                                child: Text(
-                                  "Grid View $index",
-                                  style: const TextStyle(
-                                      fontSize: 16,
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold),
-                                )),
-                          );
-                        })),
-                    Container(
-                      width: 10,
-                      color: const Color.fromRGBO(91, 91, 91, 1),
-                      child: const Center(
-                        child: Text(
-                          'Box',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 56,
-                              fontWeight: FontWeight.bold),
-                        ),
+          body: SafeArea(
+            child: Column(
+              children: [
+                const TabBar(
+                    indicatorColor: Colors.white,
+                    labelStyle: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20),
+                    indicatorWeight: 2,
+                    tabs: [
+                      Tab(
+                        text: '주식',
+                        height: 50,
                       ),
-                    ),
-                  ]))
-            ],
-          )),
+                      Tab(
+                        text: '코인',
+                        height: 50,
+                      ),
+                    ]),
+                Expanded(
+                    child: TabBarView(children: [
+                      ListView.builder(
+                          key: const PageStorageKey("LIST_VIEW"),
+                          itemCount: 1000,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              padding: const EdgeInsets.symmetric(vertical: 12),
+                              width: MediaQuery.of(context).size.width,
+                              child: Center(
+                                child: Text(
+                                  "List View $index",
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.accents[index % 15],
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            );
+                          }),
+                      GridView.builder(
+                          key: const PageStorageKey("GRID_VIEW"),
+                          itemCount: 1000,
+                          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 3,
+                            crossAxisSpacing: 12,
+                            mainAxisSpacing: 12,
+                          ),
+                          itemBuilder: ((context, index) {
+                            List<int> _number = [
+                              Random().nextInt(255),
+                              Random().nextInt(255),
+                              Random().nextInt(255)
+                            ];
+                            return Container(
+                              color:
+                              Color.fromRGBO(_number[0], _number[1], _number[2], 1),
+                              child: Center(
+                                  child: Text(
+                                    "Grid View $index",
+                                    style: const TextStyle(
+                                        fontSize: 16,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold),
+                                  )),
+                            );
+                          })),
+                    ]))
+              ],
+            )),
+          ),
     );
   }
 }
