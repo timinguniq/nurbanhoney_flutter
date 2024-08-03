f2a1b45253717e287efb0240a54db3b2a553330c
timing.uniq <timing.uniq@gmail.com>
2024-08-03 12:33:07 +0900

    add: home_page_refactor

--- lib/home/view/view.dart
+++ lib/home/view/view.dart
@@ -2,4 +2,5 @@ export 'home_body_view.dart';
 export 'honey_rank_body_view.dart';
 export 'myaccount_body_view.dart';
 export 'home_page.dart';
+export 'home_page_refactor.dart';
 export 'widgets/widgets.dart';
