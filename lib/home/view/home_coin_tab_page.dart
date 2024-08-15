0911ede4b94fdbbbde7275362b654be07fee1a57
timing.uniq <timing.uniq@gmail.com>
2024-08-15 16:07:58 +0900

    feat: backup

---
+++ lib/home/view/home_coin_tab_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeCoinTabPage extends StatelessWidget {
+  const HomeCoinTabPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
