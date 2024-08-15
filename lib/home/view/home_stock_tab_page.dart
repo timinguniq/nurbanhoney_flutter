a655c5b005ec2b8c8e10e38c80942c0ed76ef17f
timing.uniq <timing.uniq@gmail.com>
2024-08-15 16:09:46 +0900

    feat: backup

--- lib/home/view/home_stock_tab_page.dart
+++ lib/home/view/home_stock_tab_page.dart
@@ -5,6 +5,8 @@ class HomeStockTabPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Text('HomeStockTabPage'),
+    );
   }
 }
