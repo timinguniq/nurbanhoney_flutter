792ee5c14d44652969185a40a3e52ee359074e4c
timing.uniq <timing.uniq@gmail.com>
2024-12-08 14:50:35 +0900

    revise: kakao login

--- lib/login/view/login_page.dart
+++ lib/login/view/login_page.dart
@@ -351,7 +351,6 @@ class _LoginPageState extends State<LoginPage> {
         log('카카오톡으로 로그인 성공');
       } catch (error) {
         log('카카오톡으로 로그인 실패 $error');
-
         // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
         // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
         if (error is PlatformException && error.code == 'CANCELED') {
@@ -361,7 +360,6 @@ class _LoginPageState extends State<LoginPage> {
         try {
           final oAuthToken = await UserApi.instance.loginWithKakaoAccount();
           accessToken = oAuthToken.accessToken;
-          log('카카오계정으로 로그인 성공');
         } catch (error) {
           log('카카오계정으로 로그인 실패 $error');
         }
@@ -375,7 +373,6 @@ class _LoginPageState extends State<LoginPage> {
         log('카카오계정으로 로그인 실패 $error');
       }
     }
-
     return accessToken;
   }
 
