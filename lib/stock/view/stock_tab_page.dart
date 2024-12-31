8feb09f7281a4d2e4d122c4d14921c011222c64e
timing.uniq <timing.uniq@gmail.com>
2024-12-31 16:42:29 +0900

    add: backup

--- lib/stock/view/stock_tab_page.dart
+++ lib/stock/view/stock_tab_page.dart
@@ -115,6 +115,7 @@ class StockTabPage extends ConsumerWidget {
                           },
                         ),
                       ),
+                      const SizedBox(height: 80),
                     ],
                 ),
                 bottomSheet: const StockTabBottomSheetView(),
