ea3ea6f1ad261b27d604c6e10dbad2be2a3aa0e0
timing.uniq <timing.uniq@gmail.com>
2025-05-24 20:15:14 +0900

    [Update] 홈페이지 리팩토링 수정

--- lib/home/view/home_page_refactor.dart
+++ lib/home/view/home_page_refactor.dart
@@ -4,11 +4,8 @@ import 'package:authentication_service/authentication_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:navigation_service/navigation_service.dart';
-import 'package:nurbanhoney/article_create/article_create.dart';
-import 'package:nurbanhoney/gen/assets.gen.dart';
 import 'package:nurbanhoney/home/home.dart';
 import 'package:nurbanhoney/home/view/widgets/drawer_profile.dart';
-import 'package:nurbanhoney/login/login.dart';
 import 'package:nurbanhoney_ui_service/nurbanhoney_ui_service.dart';
 
 import 'package:nurbanhoney/stock/stock.dart';
@@ -54,55 +51,79 @@ class _HomeViewRefactorState extends ConsumerState<HomeViewRefactor> {
     final tabSelectedColor = ref.read(colorF6B748);
     final tabUnselectedColor = ref.read(color212124);
     final authenticationProvider = ref.watch(authenticationServiceProvider);
-
     final homeTabTextStyle = ref.read(homeTabStyle);
-
     final homeBottomNavigation = ref.watch(homeBottomNavigationProvider);
+
+    return Scaffold(
+      drawer: const DrawerProfile(),
+      body: HomeBody(
+        tabSelectedColor: tabSelectedColor,
+        tabUnselectedColor: tabUnselectedColor,
+        homeTabTextStyle: homeTabTextStyle,
+        widgetOptions: _widgetOptions,
+      ),
+    );
+  }
+}
+
+class HomeBody extends StatelessWidget {
+  final Color tabSelectedColor;
+  final Color tabUnselectedColor;
+  final TextStyle homeTabTextStyle;
+  final List<Widget> widgetOptions;
+
+  const HomeBody({
+    Key? key,
+    required this.tabSelectedColor,
+    required this.tabUnselectedColor,
+    required this.homeTabTextStyle,
+    required this.widgetOptions,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return GestureDetector(
       onPanUpdate: (details) {
         // Swiping in right direction.
         if (details.delta.dx > 0) {
+          // Drawer
           log('Swiping in right direction');
-        }
-
-        // Swiping in left direction.
-        if (details.delta.dx < 0) {
-          log('Swiping in left direction');
+          Future.delayed(Duration.zero, (){
+            if(context.mounted) Scaffold.of(context).openDrawer();
+          });
         }
       },
       child: DefaultTabController(
         length: 2,
-        child: Scaffold(
-          body: SafeArea(
-            child: Column(
-              children: [
-                TabBar(
-                  indicatorColor: tabSelectedColor,
-                  unselectedLabelColor: tabUnselectedColor,
-                  labelStyle: homeTabTextStyle,
-                  indicatorWeight: 2,
-                  tabs: const [
-                    Tab(
-                      text: '주식',
-                      height: 50,
-                    ),
-                    Tab(
-                      text: '코인',
-                      height: 50,
-                    ),
+        child: SafeArea(
+          child: Column(
+            children: [
+              TabBar(
+                indicatorColor: tabSelectedColor,
+                unselectedLabelColor: tabUnselectedColor,
+                labelStyle: homeTabTextStyle,
+                indicatorWeight: 2,
+                tabs: const [
+                  Tab(
+                    text: '주식',
+                    height: 50,
+                  ),
+                  Tab(
+                    text: '코인',
+                    height: 50,
+                  ),
+                ],
+              ),
+              Expanded(
+                child: TabBarView(
+                  physics: const NeverScrollableScrollPhysics(),
+                  children: [
+                    widgetOptions[0],
+                    widgetOptions[1],
                   ],
                 ),
-                Expanded(
-                  child: TabBarView(
-                    physics: const NeverScrollableScrollPhysics(),
-                    children: [
-                      _widgetOptions[0],
-                      _widgetOptions[1],
-                    ],
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
